aa207889c7d6af1044bfa97b401df8b3b11f5e41
MaciejGd <maciekzgk>
2025-02-22 23:33:48 +0100

    Simple toolbar added, custom event prototype

--- GUI/inc/mainwindow.h
+++ GUI/inc/mainwindow.h
@@ -2,11 +2,14 @@
 
 #include <QtWidgets>
 #include <memory>
+#include "gui_field.h"
+#include "tool_bar.h"
 
 
 class MainWindow : public QMainWindow {
     Q_OBJECT
     std::unique_ptr<GuiField> grid;
+    std::unique_ptr<ToolBar> tool_bar;
 public:
-    MainWindow(int argc, char** argv, QWidget* parent = nullptr);
+    MainWindow(int width, int height, QWidget* parent = nullptr);
 };
---
+++ GUI/src/custom_events.cpp
@@ -0,0 +1,8 @@
+#include "custom_events.h"
+
+// register event number for custom event
+const QEvent::Type PlayerMoveEvent::EventType = static_cast<QEvent::Type>(QEvent::registerEventType());
+
+
+PlayerMoveEvent::PlayerMoveEvent(int row, int col): m_row(row), m_col(col), QEvent(EventType){}
+
