950f82127b168b0d4144fbf08b390cb9c2f9872c
MaciejGd <maciekzgk>
2025-02-22 23:46:29 +0100

    names changed for more convinient

---
+++ GUI/src/GUIMainWindow.cpp
@@ -0,0 +1,14 @@
+#include "GUIMainWindow.h"
+
+
+GUIMainWindow::GUIMainWindow(int width, int height, QWidget* parent): QMainWindow(parent) {
+    
+    setFixedSize(width, height);
+
+    // set batlle field
+    m_arena = std::make_unique<GUIArena>(80, 100);
+    setCentralWidget(m_arena.get());
+    // set GUIToolBar
+    m_toolBar = std::make_unique<GUIToolBar>("Options");
+    addToolBar(m_toolBar.get());
+}
