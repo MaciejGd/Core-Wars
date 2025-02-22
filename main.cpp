de9a8aa43cc42baa03169ac49e3e08d759aff62d
MaciejGd <correct@email.com>
2025-02-22 23:33:48 +0100

    Simple toolbar added, custom event prototype

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 #include "mainwindow.h"
 
+const int WIDTH = 1200;
+const int HEIGHT = 800;
+
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
-    MainWindow window(argc, argv);
+    MainWindow window(WIDTH, HEIGHT);
     window.show();
     return app.exec();
 }
