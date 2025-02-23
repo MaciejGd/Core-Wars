cd9fff33a8b31c66ea2c28a5d98232a4e6fabaf5
MaciejGd <128997250+MaciejGd@users.noreply.github.com>
2025-02-23 22:28:28 +0100

    Simple load button handling added

--- GUI/src/GUIToolBar.cpp
+++ GUI/src/GUIToolBar.cpp
@@ -2,12 +2,15 @@
 #include <iostream>
 
 GUIToolBar::GUIToolBar(const QString& name, QWidget* parent) : QToolBar(name, parent) {
-    m_choose_btn = new QPushButton("CHOOSE_FIGHTERS");
+    m_choose_first_btn = new QPushButton("CHOOSE_FIRST_FIGHTER");
+    m_choose_second_btn = new QPushButton("CHOOSE_SECOND_FIGHTER");
     m_reset_btn = new QPushButton("RESET_ARENA");
 
-    connect(m_choose_btn, &QPushButton::pressed, this, &GUIToolBar::ChoosePlayersCb);
+    connect(m_choose_first_btn, &QPushButton::pressed, this, &GUIToolBar::ChoosePlayersCb);
+    connect(m_choose_second_btn, &QPushButton::pressed, this, &GUIToolBar::ChoosePlayersCb);
 
-    addWidget(m_choose_btn);
+    addWidget(m_choose_first_btn);
+    addWidget(m_choose_second_btn);
     addWidget(m_reset_btn);
 }
 
