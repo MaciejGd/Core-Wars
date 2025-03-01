3beaf1966534c9858d8726fb5cf1df77efa9fe08
MaciejGd <maciekzgk@email.com>
2025-03-02 00:13:04 +0100

    New textfield introduced, old implementations still not removed as it needs further testing

---
+++ GUI/inc/GUITextFieldButton.h
@@ -0,0 +1,32 @@
+#pragma once 
+
+#include <QtWidgets>
+#include <functional>
+
+
+class GUITextFieldButton : public QWidget {
+    Q_OBJECT
+public:
+    GUITextFieldButton(int btn_width, int height, const QString& btn_txt, QWidget* parent = nullptr);
+
+    void SetReadOnly(bool read_only);
+    void SetText(const QString& text);
+
+    template<typename T>
+    void SetButtonCallback(T* wgt,  void (T::*cb)()) {
+        connect(m_btn, &QPushButton::pressed, wgt, cb);
+    };
+private:
+
+    QTextEdit* m_textfield;
+    QPushButton* m_btn;
+
+    std::string m_player_path;
+    QString m_btn_txt;
+    int m_btn_width;
+    int m_height;
+
+    void m_InitTextField();
+    void m_InitButton();
+
+};
