af0b196c1ee5381991d3baf6f673fb2f30fe6083
MaciejGd <maciekzgk@email.com>
2025-03-02 21:37:50 +0100

    Simple logger added, two versions, currently used c++20 style

---
+++ logger/logger.h
@@ -0,0 +1,83 @@
+#pragma once
+
+#include <stdio.h>
+#include <string>
+#include <cstring>
+#include <filesystem>
+#include <string_view>
+#include <iostream>
+#include <format>
+
+#define PREFIX_LEN 10
+#define FILENAME_LEN 40
+
+#define FILENAME std::filesystem::path(__FILE__).filename().string()
+
+enum TextColor 
+{
+    RED,
+    WHITE,
+    YELLOW,
+    TEXT_COLOR_COUNT
+};
+
+
+//std::cout << __FILE__ << "(" << __LINE__ << "): " << string(80-strlen(__FILE__), ' ')  << msg << std::endl
+template<typename ...Args>
+void _log(std::string_view prefix, std::string_view msg, TextColor color, std::string_view filename, int fileline, Args... args) 
+{
+    static const char* textColorTable[TEXT_COLOR_COUNT] = 
+    {
+        "\033[31m", // color code for red
+        "\033[0m",  // color code for white
+        "\033[33m"  // color code for yellow
+    };
+
+    std::string prefix_spacing = std::string(PREFIX_LEN - prefix.size(), ' ');
+
+    std::string filename_part = std::format("{}:{}", filename, fileline);
+    std::string filename_spacing = std::string(FILENAME_LEN - filename_part.size(), ' ');
+    filename_part += filename_spacing;
+
+    std::string formatted_msg = std::vformat(msg, std::make_format_args(args...));
+    std::string output = std::format("{} [{}]{}{} {} \033[0m", 
+        textColorTable[color], prefix, prefix_spacing.c_str(), filename_part.c_str(), formatted_msg);
+
+
+    std::cout << output.c_str() << "\n";
+    //std::puts(output.c_str());
+};
+
+#define LOG_DBG(msg, ...) _log("DEBUG", msg, TextColor::WHITE, FILENAME, __LINE__, ##__VA_ARGS__);
+#define LOG_ERR(msg, ...) _log("ERROR", msg, TextColor::RED, FILENAME, __LINE__, ##__VA_ARGS__);
+#define LOG_WRN(msg, ...) _log("WARN",  msg, TextColor::YELLOW, FILENAME, __LINE__, ##__VA_ARGS__);
+
+// template<typename ...Args>
+// void _log(const char* prefix, const char* msg, TextColor color, std::string_view filename_, int fileline, Args... args) 
+// {
+//     static const char* textColorTable[TEXT_COLOR_COUNT] = 
+//     {
+//         "\033[31m", // color code for red
+//         "\033[0m",  // color code for white
+//         "\033[33m"  // color code for yellow
+//     };
+
+//     std::string prefix_spacing = std::string(PREFIX_LEN - strlen(prefix), ' ');
+
+//     std::string filename = std::string(filename_);
+//     std::string filename_part = std::string(filename) + ":" + std::to_string(fileline);
+//     std::string filename_spacing = std::string(FILENAME_LEN - filename_part.size(), ' ');
+//     filename_part += filename_spacing;
+
+//     char formatBuffer[8192] = {};
+//     sprintf(formatBuffer, "%s [%s]%s%s %s \033[0m", textColorTable[color], prefix, prefix_spacing.c_str(), filename_part.c_str(), msg);
+
+//     char textBuffer[8192] = {};
+//     sprintf(textBuffer, formatBuffer, args...);
+
+//     puts(textBuffer);
+// };
+
+// #define LOG_DBG(msg, ...) _log("DEBUG", msg, TextColor::WHITE, FILENAME, __LINE__, ##__VA_ARGS__);
+// #define LOG_ERR(msg, ...) _log("ERROR", msg, TextColor::RED, FILENAME, __LINE__, ##__VA_ARGS__);
+// #define LOG_WRN(msg, ...) _log("WARN",  msg, TextColor::YELLOW, FILENAME, __LINE__, ##__VA_ARGS__);
