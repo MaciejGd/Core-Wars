e0afb41f64c32480075cf5e1f47e3fbfd2d67827
MaciejGd <128997250+MaciejGd@users.noreply.github.com>
2025-03-30 22:00:23 +0200

    Arithmetic operations implemented

--- Logic/Instructions/Operations/src/OperationMOD.cpp
+++ Logic/Instructions/Operations/src/OperationMOD.cpp
@@ -1,4 +1,8 @@
 #include "OperationMOD.h"
+#include "Arena.h"
+#include "Instruction.h"
+#include "Parameter.h"
+#include "logger.h"
 
 COperationMOD::COperationMOD()
 {
@@ -12,5 +16,75 @@ std::unique_ptr<COperation> COperationMOD::clone() const
 
 bool COperationMOD::Execute(std::unique_ptr<CParameter> &A_param, std::unique_ptr<CParameter> &B_param, int &pc)
 {
-    return false;
+    LOG_DBG("Executing {}.{} in memory cell {}", m_name, ModifierToString(m_modifier), pc);
+    CArena& arena = CArena::GetInstance();
+    int a_pointer = A_param->GetPointer();
+    int b_pointer = B_param->GetPointer();
+
+    auto& IRB = arena[pc + b_pointer];
+    auto& IRA = arena[pc + a_pointer];
+
+    int IRA_A_Param = IRA->GetAParamValue();
+    int IRA_B_Param = IRA->GetBParamValue();
+
+    switch (m_modifier)
+    {
+        case A:
+            // set value A IRB to value A of IRB + value A of IRA
+            if (IRA_A_Param == 0) 
+            {
+                LOG_ERR("Trying to divide by 0");
+                return false;
+            }
+            IRB->SetAParamValue((IRB->GetAParamValue() % IRA->GetAParamValue()));
+            break;
+        case B:
+            if (IRA_B_Param == 0) 
+            {
+                LOG_ERR("Trying to divide by 0");
+                return false;
+            }
+            IRB->SetBParamValue((IRB->GetBParamValue() % IRA->GetBParamValue()));
+            break;
+        case AB:
+            if (IRA_B_Param == 0) 
+            {
+                LOG_ERR("Trying to divide by 0");
+                return false;
+            }
+            IRB->SetBParamValue((IRB->GetAParamValue() % IRA->GetBParamValue()));
+            break;
+        case BA:
+            if (IRA_A_Param == 0) 
+            {
+                LOG_ERR("Trying to divide by 0");
+                return false;
+            }
+            IRB->SetAParamValue((IRB->GetBParamValue() % IRA->GetAParamValue()));
+            break;
+        case X:
+            if (IRA_A_Param == 0 || IRA_B_Param == 0) 
+            {
+                LOG_ERR("Trying to divide by 0");
+                return false;
+            }
+            IRB->SetAParamValue((IRB->GetBParamValue() % IRA->GetAParamValue()));
+            IRB->SetBParamValue((IRB->GetAParamValue() % IRA->GetBParamValue()));
+            break;  
+        case F:
+        case I:
+            if (IRA_A_Param == 0 || IRA_B_Param == 0) 
+            {
+                LOG_ERR("Trying to divide by 0");
+                return false;
+            }
+            IRB->SetAParamValue((IRB->GetAParamValue() % IRA->GetAParamValue()));
+            IRB->SetBParamValue((IRB->GetBParamValue() % IRA->GetBParamValue()));
+            break;
+        default: 
+            LOG_ERR("Undefined parameter in {}", m_name);
+            return false;
+    }
+    pc = (pc + 1) % ARENA_SIZE;
+    return true;
 }
--- Logic/Instructions/Operations/src/OperationSUB.cpp
+++ Logic/Instructions/Operations/src/OperationSUB.cpp
@@ -1,4 +1,8 @@
 #include "OperationSUB.h"
+#include "Arena.h"
+#include "Instruction.h"
+#include "Parameter.h"
+#include "logger.h"
 
 COperationSUB::COperationSUB()
 {
@@ -12,5 +16,42 @@ std::unique_ptr<COperation> COperationSUB::clone() const
 
 bool COperationSUB::Execute(std::unique_ptr<CParameter> &A_param, std::unique_ptr<CParameter> &B_param, int &pc)
 {
-    return false;
+    LOG_DBG("Executing {}.{} in memory cell {}", m_name, ModifierToString(m_modifier), pc);
+    CArena& arena = CArena::GetInstance();
+    int a_pointer = A_param->GetPointer();
+    int b_pointer = B_param->GetPointer();
+
+    auto& IRB = arena[pc + b_pointer];
+    auto& IRA = arena[pc + a_pointer];
+
+    switch (m_modifier)
+    {
+        case A:
+            // set value A IRB to value A of IRB + value A of IRA
+            IRB->SetAParamValue((IRB->GetAParamValue() + ARENA_SIZE - IRA->GetAParamValue()) % ARENA_SIZE);
+            break;
+        case B:
+            IRB->SetBParamValue((IRB->GetBParamValue() + ARENA_SIZE - IRA->GetBParamValue()) % ARENA_SIZE);
+            break;
+        case AB:
+            IRB->SetBParamValue((IRB->GetAParamValue() + ARENA_SIZE - IRA->GetBParamValue()) % ARENA_SIZE);
+            break;
+        case BA:
+            IRB->SetAParamValue((IRB->GetBParamValue() + ARENA_SIZE - IRA->GetAParamValue()) % ARENA_SIZE);
+            break;
+        case X:
+            IRB->SetAParamValue((IRB->GetBParamValue() + ARENA_SIZE - IRA->GetAParamValue()) % ARENA_SIZE);
+            IRB->SetBParamValue((IRB->GetAParamValue() + ARENA_SIZE - IRA->GetBParamValue()) % ARENA_SIZE);
+            break;  
+        case F:
+        case I:
+            IRB->SetAParamValue((IRB->GetAParamValue() + ARENA_SIZE - IRA->GetAParamValue()) % ARENA_SIZE);
+            IRB->SetBParamValue((IRB->GetBParamValue() + ARENA_SIZE - IRA->GetBParamValue()) % ARENA_SIZE);
+            break;
+        default: 
+            LOG_ERR("Undefined parameter in {}", m_name);
+            return false;
+    }
+    pc = (pc + 1) % ARENA_SIZE;
+    return true;
 }
